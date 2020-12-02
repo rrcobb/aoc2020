2ab25e541cda53b3e78ee43a2b8a7e4714ded288
rob cobb <rwcobbjr@gmail.com>
2020-12-02 17:08:33 -0500

    part 1, day 1

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,4 +1,28 @@
 use structopt::StructOpt;
+use anyhow::{Context, Result};
+
+fn one(path: Option<std::path::PathBuf>) -> Result<()> {
+    let unwrapped_path = path.context("This example needs a path").unwrap();
+    let content = std::fs::read_to_string(&unwrapped_path)
+        .with_context(|| format!("could not read from {}", unwrapped_path.display()))?;
+
+    // turn content from string to array of nums
+    let nums = content
+        .split("\n")
+        .map(|s| s.parse::<i32>())
+        .filter_map(Result::ok)
+        .collect::<Vec<i32>>();
+
+    'outer: for num in &nums {
+        for other in &nums {
+            if num + other == 2020 {
+                println!("{}", num * other);
+                break 'outer;
+            }
+        }
+    }
+    Ok(())
+}
 
 #[derive(StructOpt)]
 struct Cli {
@@ -9,5 +33,10 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    print!("{} {:?}", args.pattern, args.path);
+    let _ = match args.pattern.as_str() {
+        "1" => one(args.path),
+        "one" => one(args.path),
+        _ => Ok(()),
+    };
+    ()
 }
