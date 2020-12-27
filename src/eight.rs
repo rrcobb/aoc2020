17079587ae940540472dc131f34602c8bbf97b94
rob cobb <rwcobbjr@gmail.com>
2020-12-27 10:00:28 -0500

    more brute force

--- src/eight.rs
+++ src/eight.rs
@@ -1,11 +1,11 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use std::collections::HashSet;
 use std::convert::TryInto;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Keyword { Acc, Jmp, Nop, }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Instruction {
     keyword: Keyword,
     arg: i32,
@@ -38,23 +38,17 @@ peg::parser!{
     }
 }
 
-pub fn eight() -> Result<()> {
-    let content = include_str!("input/eight.txt");
-    let instructions: Vec<Instruction> = content
-        .lines()
-        .map(|line| instruction_parser::instruction(line))
-        .filter_map(Result::ok)
-        .collect();
-
+fn run_program(instructions: &[Instruction]) -> Result<(bool, usize, i32)> {
     let mut visited: HashSet<usize> = HashSet::new();
     let mut accumulator: i32 = 0;
     let mut pc: usize = 0;
-    
+    let mut success = false;
+
     loop {
         if visited.contains(&pc) { break; }
+        if pc == instructions.len() { success = true; break; }
         visited.insert(pc);
-        let instruction = instructions.get(pc).expect("pc not in bounds of program");
-        dbg!(pc, instruction);
+        let instruction = instructions.get(pc).ok_or(anyhow!("pc out of bounds {}, {}", pc, accumulator))?;
         match &instruction.keyword {
             Keyword::Nop => { pc += 1; },
             Keyword::Acc => {
@@ -64,7 +58,62 @@ pub fn eight() -> Result<()> {
             Keyword::Jmp => { pc = ((pc as i32) + instruction.arg).try_into().unwrap() },
         }
     }
-    dbg!(pc, accumulator, visited.len());
+    Ok((success, pc, accumulator))
+}
 
+pub fn eight() -> Result<()> {
+    let content = include_str!("input/eight.txt");
+    let instructions: Vec<Instruction> = content
+        .lines()
+        .map(|line| instruction_parser::instruction(line))
+        .filter_map(Result::ok)
+        .collect();
+
+    let variants: Vec<Vec<Instruction>> = instructions
+        .iter()
+        .enumerate()
+        .filter(|(_, instr)| instr.keyword == Keyword::Jmp || instr.keyword == Keyword::Nop)
+        .map(|(index, instr)| {
+            let mut variant = instructions.clone();
+            variant[index].keyword = match instr.keyword {
+                Keyword::Jmp => Keyword::Nop,
+                Keyword::Nop => Keyword::Jmp,
+                Keyword::Acc => Keyword::Acc,
+            };
+            variant
+        })
+    .collect();
+
+
+    variants
+        .iter()
+        .map(|list| run_program(list))
+        .filter_map(|res| {
+            match res {
+                Ok(x) => Some(x),
+                Err(message) => {
+                    dbg!(message);
+                    None 
+                }
+            }
+        })
+        .filter(|(success, _, _)| *success)
+        .for_each(|(success, pc, accumulator)| {
+            dbg!(success, pc, accumulator);
+        });
+    
     Ok(())
 }
+
+
+// plan:
+// instructions form a graph with one cycle
+// each instruction has a 'next' instruction
+// each nop and jmp as an alternate next instruction
+// how can we find the one alternate instruction to use that avoids a cycle?
+// ideas:
+//  - sections of code that can or can't be escaped from
+//  - some kind of graph traversal algorithm
+//  - weird stateful graph, where we keep track of whether we've taken a alt or not, and backtrack
+//  if we hit a cycle then?
+//  - check them all
